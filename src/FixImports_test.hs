d16958c7b89e7821030e81507b8b125e90ff1fb1
Evan Laforge <qdunkan@gmail.com>
2020-05-10 14:52:41 -0700

    fix tests

--- src/FixImports_test.hs
+++ src/FixImports_test.hs
@@ -3,6 +3,7 @@
 module FixImports_test where
 import Control.Monad (unless, void)
 import qualified Control.Monad.Identity as Identity
+import qualified Control.Monad.State.Strict as State
 import Data.Bifunctor (bimap)
 import qualified Data.List as List
 import qualified Data.Map as Map
@@ -23,33 +24,27 @@ import EL.Test.Global
 
 
 test_simple = do
-    let run config = fmap FixImports.resultText
+    let run config = fmap FixImports.resultImports
             . fixModule index ["C.hs"] (mkConfig config) "A.hs"
         index = Index.makeIndex
             [ ("pkg", ["A.B"])
             , ("zpkg", ["Z"])
             ]
-    equal (run "" "x = B.c") $ Right
-        "import qualified A.B as B\n\
-        \x = B.c\n"
-    equal (run "" "x = A.B.c") $ Right
-        "import qualified A.B\n\
-        \x = A.B.c\n"
+    equal (run "" "x = B.c") $ Right "import qualified A.B as B\n"
+    equal (run "" "x = A.B.c") $ Right "import qualified A.B\n"
     leftLike (run "" "x = Q.c") "not found: Q"
     -- Remove unused.
-    equal (run "" "import qualified A.B as B\n\nx = y") $ Right
-        "\nx = y\n"
+    equal (run "" "import qualified A.B as B\n\nx = y") $ Right ""
     -- Unless it's unqualified.
     equal (run "" "import A.B as B\n\nx = y") $ Right
-        "import A.B as B\n\nx = y\n"
+        "import A.B as B\n"
 
     -- Local goes below package.
     equal (run "" "x = (B.a, C.a, Z.a)") $ Right
         "import qualified A.B as B\n\
         \import qualified Z\n\
         \\n\
-        \import qualified C\n\
-        \x = (B.a, C.a, Z.a)\n"
+        \import qualified C\n"
 
     -- Don't mess with imports I don't manage.
     equal (run "" "import A.B hiding (mod)\n") $
@@ -64,7 +59,7 @@ test_qualifyAs = do
     equal (run config [] "x = DTL.y") $ Right
         ( ["Data.Text.Lazy"]
         , []
-        , "import qualified Data.Text.Lazy as DTL\nx = DTL.y\n"
+        , "import qualified Data.Text.Lazy as DTL\n"
         )
     equal (run config [] "import qualified Data.Text.Lazy as DTL\n") $ Right
         ( []
@@ -75,7 +70,7 @@ test_qualifyAs = do
     -- qualifyAs aliases are prioritized in the same way as others, so
     -- the local module wins:
     equal (run config ["DTL.hs"] "x = DTL.y") $
-        Right (["DTL"], [], "import qualified DTL\nx = DTL.y\n")
+        Right (["DTL"], [], "import qualified DTL\n")
 
     -- Unless explicitly suppressed:
     let config2 = mkConfig $ Text.unlines
@@ -85,7 +80,7 @@ test_qualifyAs = do
     equal (run config2 ["DTL.hs"] "x = DTL.y") $ Right
         ( ["Data.Text.Lazy"]
         , []
-        , "import qualified Data.Text.Lazy as DTL\nx = DTL.y\n"
+        , "import qualified Data.Text.Lazy as DTL\n"
         )
 
 test_unqualified = do
@@ -95,64 +90,60 @@ test_unqualified = do
             [ ("pkg", ["A.B"])
             , ("zpkg", ["Z"])
             ]
-    equal (run "unqualified: A.B (c)" "x = (c, c)") $ Right (["A.B"], [],
-        "import A.B (c)\n\
-        \x = (c, c)\n"
+    equal (run "unqualified: A.B (c)" "x = (c, c)") $ Right
+        ( ["A.B"]
+        , []
+        , "import A.B (c)\n"
         )
 
     -- Modify an existing import.
-    equal (run "unqualified: A.B (c)" "import A.B (a, z)\nx = (c, c)") $
-        Right ([], [],
-        "import A.B (a, c, z)\n\
-        \x = (c, c)\n"
+    equal (run "unqualified: A.B (c)" "import A.B (a, z)\nx = (c, c)") $ Right
+        ( []
+        , []
+        , "import A.B (a, c, z)\n"
         )
-    equal (run "unqualified: A.B (a, c)" "import A.B (a, c)\nx = a") $
-        Right ([], [],
-        "import A.B (a)\n\
-        \x = a\n"
+    equal (run "unqualified: A.B (a, c)" "import A.B (a, c)\nx = a") $ Right
+        ( []
+        , []
+        , "import A.B (a)\n"
         )
     -- Don't accumulate duplicates.
     equal (run "unqualified: A.B (c)" "import A.B (c)\nx = c") $
-        Right ([], [], "import A.B (c)\nx = c\n")
+        Right ([], [], "import A.B (c)\n")
     equal (run "unqualified: A.B (C)" "import A.B (C)\nx :: C") $
-        Right ([], [],
-        "import A.B (C)\n\
-        \x :: C\n"
-        )
+        Right ([], [], "import A.B (C)\n")
 
     -- Don't manage it if it's not mine.
     equal (run "unqualified: A.B (c)" "import A.B (d)") $ Right
         ([], [], "import A.B (d)\n")
 
     -- local still goes below package
-    equal (run "unqualified: C (a)" "import A.B\nimport Z\nx = a") $
-        Right (["C"], [],
-        "import A.B\n\
-        \import Z\n\
-        \\n\
-        \import C (a)\n\
-        \x = a\n"
+    equal (run "unqualified: C (a)" "import A.B\nimport Z\nx = a") $ Right
+        ( ["C"]
+        , []
+        , "import A.B\n\
+          \import Z\n\
+          \\n\
+          \import C (a)\n"
         )
 
     -- Don't import when it's an assignee.
-    equal (run "unqualified: A.B (c)" "c = x") $ Right ([], [], "c = x\n")
-    equal (run "unqualified: A.B ((</>))" "x = a </> b") $ Right (["A.B"], [],
-        "import A.B ((</>))\n\
-        \x = a </> b\n"
-        )
+    equal (run "unqualified: A.B (c)" "c = x") $ Right ([], [], "")
+    equal (run "unqualified: A.B ((</>))" "x = a </> b") $
+        Right (["A.B"], [], "import A.B ((</>))\n")
 
     -- Removed unused.
     equal (run "unqualified: A.B (c)" "import A.B (c)\nx = x\n") $
-        Right ([], ["A.B"], "x = x\n")
+        Right ([], ["A.B"], "")
     -- But not if it's a spec-less import.
     equal (run "unqualified: A.B (c)" "import A.B\nx = x\n") $
-        Right ([], [], "import A.B\nx = x\n")
+        Right ([], [], "import A.B\n")
 
 eResult :: FixImports.Result -> ([Types.ModuleName], [Types.ModuleName], String)
 eResult r =
     ( Set.toList (FixImports.resultAdded r)
     , Set.toList (FixImports.resultRemoved r)
-    , FixImports.resultText r
+    , FixImports.resultImports r
     )
 
 fixModule :: Index.Index -> [FilePath]
@@ -161,9 +152,10 @@ fixModule index files config modulePath source =
     case FixImports.parse (Config._language config) modulePath source of
         Haskell.ParseFailed srcloc err ->
             Left $ Haskell.prettyPrint srcloc ++ ": " ++ err
-        Haskell.ParseOk (mod, cmts) -> Identity.runIdentity $
+        Haskell.ParseOk (mod, cmts) ->
+            fst $ Identity.runIdentity $ flip State.runStateT [] $
             FixImports.fixImports (pureFilesystem files) config index
-                modulePath mod cmts source
+                modulePath mod cmts
 
 -- | The ./ stuff is tricky, this is probably still wrong.
 pureFilesystem :: [FilePath] -> FixImports.Filesystem Identity.Identity
@@ -200,32 +192,6 @@ mkConfig content
     | otherwise = error $ "parsing " <> show content  <> ": "
         <> unlines (map Text.unpack errs)
     where (config, errs) = Config.parse content
-
-tmod0 :: String
-tmod0 =
-    "-- cmt1\n\
-    \-- cmt2\n\
-    \import Data.List\n\
-    \x = 42\n"
-
-tmod1 = "module Foo\nwhere\n" ++ tmod0
-tmod2 = "module Foo\nwhere\nx = 42\n"
-
-tmod :: String
-tmod =
-    "{-# LANGUAGE SomeExtension #-} -- comment on LANGUAGE\n\
-    \-- cmt1 for Data.List\n\
-    \-- cmt2 for Data.List\n\
-    \import qualified Data.List as C\n\
-    \-- cmt1 for Util\n\
-    \-- cmt2 for Util\n\
-    \import qualified Util -- cmt right of Util\n\
-    \import qualified Extra as Biz {- block cmt -}\n\
-    \import Data.Map (a,\n\
-    \       b)\n\
-    \\n\
-    \f :: Util.One -> Midi.New.Two -> Util.Foo -> C.Result\n\
-    \f x = x * C.z\n"
 
 -- * util
 
